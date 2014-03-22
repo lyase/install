0c8cf175d1462a0548f9a32133975d96ddbf3edd
WittyWizard <admin@wittywizard.org>
2014-03-22 12:45:38 -0700

    update BlogRSSFeed.h

---
+++ BlogRSSFeed.h
@@ -0,0 +1,30 @@
+// This may look like C code, but it's really -*- C++ -*-
+/*
+ * Copyright (C) 2009 Emweb bvba, Kessel-Lo, Belgium.
+ *
+ * See the LICENSE file for terms of use.
+ */
+#ifndef BLOG_RSS_FEED_H_
+#define BLOG_RSS_FEED_H_
+
+#include <Wt/WResource>
+
+/* ****************************************************************************
+ * BlogSession
+ */
+class BlogSession;
+/* ****************************************************************************
+ * BlogRSSFeed
+ */
+class BlogRSSFeed : public Wt::WResource
+{
+    public:
+        BlogRSSFeed();
+        virtual ~BlogRSSFeed();
+
+    protected:
+        virtual void handleRequest(const Wt::Http::Request &request, Wt::Http::Response &response);
+};
+
+#endif // BLOG_RSS_FEED_H_
+// --- End Of File ------------------------------------------------------------
