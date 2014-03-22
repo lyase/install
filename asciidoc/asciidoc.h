8895fd3a5400218695af7c1da8d5a3e69f485c11
WittyWizard <admin@wittywizard.org>
2014-03-22 12:45:38 -0700

    update asciidoc

---
+++ asciidoc/asciidoc.h
@@ -0,0 +1,17 @@
+#ifndef ASCIIDOC_H_
+#define ASCIIDOC_H_
+
+/* ****************************************************************************
+ * WString
+ */
+namespace Wt
+{
+    class WString;
+}
+/* ****************************************************************************
+ * asciidoc
+ */
+extern Wt::WString asciidoc(const Wt::WString& src);
+
+#endif // ASCIIDOC_H_
+// --- End Of File ------------------------------------------------------------
