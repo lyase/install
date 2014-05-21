ba9a08a4d9ccecb266d766c51e034feed4f6b302
WittyWizard <admin@wittywizard.org>
2014-05-20 22:46:08 -0700

    update view

--- view/VideoView.cpp
+++ view/VideoView.cpp
@@ -1,5 +1,23 @@
+/* ****************************************************************************
+ * Video View
+ */
+#ifdef VIDEOMAN
 #include "VideoView.h"
 
-VideoView::VideoView()
+/* ****************************************************************************
+ * Video View
+ */
+VideoView::VideoView(const std::string& appPath, const std::string& basePath, Wt::Dbo::SqlConnectionPool& db)
 {
-}
+    impl_ = new VideoImpl(appPath, basePath, db);
+    setImplementation(impl_);
+} // end
+/* ****************************************************************************
+ * set Internal Base Path
+ */
+void VideoView::setInternalBasePath(const std::string& basePath)
+{
+    impl_->setInternalBasePath(basePath);
+} // end
+#endif // VIDEOMAN
+// --- End Of File ------------------------------------------------------------
--- view/VideoView.h
+++ view/VideoView.h
@@ -1,10 +1,27 @@
+/* ****************************************************************************
+ * Video View
+ */
+#ifdef VIDEOMAN
 #ifndef VIDEOVIEW_H
 #define VIDEOVIEW_H
 
-class VideoView
+#include <Wt/WCompositeWidget>
+#include "VideoImpl.h"
+/* ****************************************************************************
+ * Prototype VideoImpl
+ */
+class VideoImpl;
+/* ****************************************************************************
+ * Video View
+ */
+class VideoView : public Wt::WCompositeWidget
 {
     public:
-        VideoView();
+        VideoView(const std::string& appPath, const std::string& basePath, Wt::Dbo::SqlConnectionPool& db);
+        void setInternalBasePath(const std::string& basePath);
+    private:
+        VideoImpl *impl_;
 };
-
 #endif // VIDEOVIEW_H
+#endif // VIDEOMAN
+// --- End Of File ------------------------------------------------------------
