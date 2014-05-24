8518c026c0057296ff43e7acdb9fb63823f72a56
WittyWizard <admin@wittywizard.org>
2014-05-23 23:40:57 -0700

    update model

--- model/BlogSession.cpp
+++ model/BlogSession.cpp
@@ -2,6 +2,9 @@
  * Copyright (C) 2008 Emweb bvba, Kessel-Lo, Belgium.
  *
  * See the LICENSE file for terms of use.
+ *
+ * Modified for Witty Wizard
+ *
  */
 #include <Wt/Auth/AuthService>
 #include <Wt/Auth/HashFunction>
--- model/HitCounter.h
+++ model/HitCounter.h
@@ -1,10 +1,35 @@
 #ifndef HITCOUNTER_H
 #define HITCOUNTER_H
+#include <Wt/WApplication>
+#include <Wt/WContainerWidget>
+#include <Wt/WDateTime>
+#include <Wt/Dbo/Dbo>
+#include <QString>
+#include <vector>
 
-class HitCounter
+/* ****************************************************************************
+ * Hit Counter
+ * ipaddress: ip4 or ip6, hash not acceptable for counting unique hits
+ * hits: Perpage
+ */
+class HitCounter : public Wt::Dbo::Dbo<HitCounter>
 {
     public:
         HitCounter();
+        Wt::WString ipaddress;    // ipaddress
+        Wt::WString page;         // page
+        int hits;                 // hits
+        Wt::WDateTime  date;
+
+        //
+        template<class Action>
+        void persist(Action &a)
+        {
+            Wt::Dbo::field(a, ipaddress,       "ipaddress");
+            Wt::Dbo::field(a, hits,            "hits");
+            Wt::Dbo::field(a, page,            "page");
+        }
 };
 
 #endif // HITCOUNTER_H
+// --- End Of File ------------------------------------------------------------
