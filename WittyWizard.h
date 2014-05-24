cdeeaeeb030e97eb5eebc9d4909a3f139ac8cd75
WittyWizard <admin@wittywizard.org>
2014-05-23 23:40:57 -0700

    update WittyWizard.h

--- WittyWizard.h
+++ WittyWizard.h
@@ -1,10 +1,12 @@
 #ifndef WITTYWIZARD_H
 #define WITTYWIZARD_H
 
-class WittyWizard
-{
-    public:
-        WittyWizard();
-};
+// Set Cookie
+void SetCookie(std::string name, std::string myValue);
+// Get Cookie
+std::string GetCookie(std::string name);
+// String Replace
+bool StringReplace(std::string& string2replace, const std::string& changefrom, const std::string& changeTo);
 
 #endif // WITTYWIZARD_H
+// --- End Of File ------------------------------------------------------------
