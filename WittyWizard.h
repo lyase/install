82e1651db468132bc7197bbc87c375894873ddf3
WittyWizard <admin@wittywizard.org>
2014-05-28 20:40:26 -0700

    update WittyWizard.h

--- WittyWizard.h
+++ WittyWizard.h
@@ -1,12 +1,17 @@
 #ifndef WITTYWIZARD_H
 #define WITTYWIZARD_H
-
+#include <Wt/WApplication>
+#include <Wt/WEnvironment>
+#include <Wt/WLogger>
+#include <iomanip>
+#include <locale>
 // Set Cookie
 void SetCookie(std::string name, std::string myValue);
 // Get Cookie
 std::string GetCookie(std::string name);
 // String Replace
 bool StringReplace(std::string& string2replace, const std::string& changefrom, const std::string& changeTo);
-
+//
+std::string FormatWithCommas(long value, std::string myLocale);
 #endif // WITTYWIZARD_H
 // --- End Of File ------------------------------------------------------------
