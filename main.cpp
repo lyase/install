ace7357f4f796834cfd078298ee401e763c1e876
WittyWizard <admin@wittywizard.org>
2014-07-12 00:02:46 -0700

    update main.cpp

--- main.cpp
+++ main.cpp
@@ -11,7 +11,6 @@
 
 #include <Wt/WServer>
 //#include <boost/filesystem.hpp>
-#include <sys/stat.h>
 #include "HomeFundation.h"
 #include "SimpleChat.h"
 #include <map>
@@ -97,43 +96,31 @@ std::map <std::string, std::string> myIncludes;
  */
 std::map <std::string, std::string> myDefaultTheme;
 /* ****************************************************************************
- * Global Function
- * is File: Full Path to File: /home/domain.tdl/filename.ext
+ * Global Variable
+ * Used to create database, turn off afterwards so you do not get errors
+ * FIXIT make admin function to change this on the fly *
  */
-inline bool isFile(const std::string& name)
-{
-  struct stat buffer;
-  return (stat (name.c_str(), &buffer) == 0);
-  // which is faster?
-  // return boost::filesystem::exists(name);
-}
+bool initDb = false;
+std::string MyVersion = "1.0.0";
 /* ****************************************************************************
- * Global Function
- * is Path: Full Path: /home/domain.tdl/
+ * Global functions
  */
-struct stat sb;
-inline bool isPath(const std::string& pathName)
-{
-    if (stat(pathName.c_str(), &sb) == 0 && S_ISDIR(sb.st_mode))
-    {
-        return true;
-    }
-    return false;
-}
+extern bool isFile(const std::string& name);
+extern bool isPath(const std::string& pathName);
 /* ****************************************************************************
  * makeConnectionPool
  * FIXIT: Would it be faster to save this to a database?
  */
-bool makeConnectionPool(QString filePath)
+bool getConnectionPoolInfo(QString filePath)
 {
     if (isFile(filePath.toStdString()))
     {
-        Wt::log("info") << "Starting XML Configuration File makeConnectionPool(" << filePath.toStdString() << ")";
+        Wt::log("info") << "Starting version: " << MyVersion << " XML Configuration File makeConnectionPool(" << filePath.toStdString() << ")";
     }
     else
     {
         Wt::log("error") << "-> Missing XML Configuration File makeConnectionPool(" << filePath.toStdString() << ")";
-        return 1;
+        return false;
     }
     // Open XML File
     rapidxml::file<> xmlFile(filePath.toUtf8().constData());
@@ -150,7 +137,7 @@ bool makeConnectionPool(QString filePath)
         if (!x_item)
         {
             Wt::log("error") << "-> makeConnectionPool(" << filePath.toStdString() << ") Missing XML Element: domainHost = " << domain_node->name();
-            return 1;
+            return false;
         }
         std::string domainHost(x_item->value(), x_item->value_size());
         myDomainHost[domainHost] = domainHost;
@@ -159,7 +146,7 @@ bool makeConnectionPool(QString filePath)
         if (!x_item)
         {
             Wt::log("error") << "-> makeConnectionPool(" << filePath.toStdString() << ") Missing XML Element: domainPath";
-            return 1;
+            return false;
         }
         std::string domainPath(x_item->value(), x_item->value_size());
         myDomainPath[domainHost] = domainPath;
@@ -172,7 +159,7 @@ bool makeConnectionPool(QString filePath)
         if (!x_item)
         {
             Wt::log("error") << "-> makeConnectionPool(" << filePath.toStdString() << ") Missing XML Element: dbName";
-            return 1;
+            return false;
         }
         std::string dbName(x_item->value(), x_item->value_size());
         myDbName[domainHost] = dbName;
@@ -181,7 +168,7 @@ bool makeConnectionPool(QString filePath)
         if (!x_item)
         {
             Wt::log("error") << "-> makeConnectionPool(" << filePath.toStdString() << ") Missing XML Element: dbUser";
-            return 1;
+            return false;
         }
         std::string dbUser(x_item->value(), x_item->value_size());
         myDbUser[domainHost] = dbUser;
@@ -190,7 +177,7 @@ bool makeConnectionPool(QString filePath)
         if (!x_item)
         {
             Wt::log("error") << "-> makeConnectionPool(" << filePath.toStdString() << ") Missing XML Element: dbPassword";
-            return 1;
+            return false;
         }
         std::string dbPassword(x_item->value(), x_item->value_size());
         myDbPassword[domainHost] = dbPassword;
@@ -199,7 +186,7 @@ bool makeConnectionPool(QString filePath)
         if (!x_item)
         {
             Wt::log("error") << "-> makeConnectionPool(" << filePath.toStdString() << ") Missing XML Element: dbPort";
-            return 1;
+            return false;
         }
         std::string dbPort(x_item->value(), x_item->value_size());
         myDbPort[domainHost] = dbPort;
@@ -208,7 +195,7 @@ bool makeConnectionPool(QString filePath)
         if (!x_item)
         {
             Wt::log("error") << "-> makeConnectionPool(" << filePath.toStdString() << ") Missing XML Element: gaAccount";
-            return 1;
+            return false;
         }
         std::string gaAccount(x_item->value(), x_item->value_size());
         myGaAccount[domainHost] = gaAccount;
@@ -217,7 +204,7 @@ bool makeConnectionPool(QString filePath)
         if (!x_item)
         {
             Wt::log("error") << "-> makeConnectionPool(" << filePath.toStdString() << ") Missing XML Element: gasAccount";
-            return 1;
+            return false;
         }
         std::string gasAccount(x_item->value(), x_item->value_size());
         myGasAccount[domainHost] = gasAccount;
@@ -226,7 +213,7 @@ bool makeConnectionPool(QString filePath)
         if (!x_item)
         {
             Wt::log("error") << "-> makeConnectionPool(" << filePath.toStdString() << ") Missing XML Element: myIncludes";
-            return 1;
+            return false;
         }
         std::string includes(x_item->value(), x_item->value_size());
         myIncludes[domainHost] = includes;
@@ -235,7 +222,7 @@ bool makeConnectionPool(QString filePath)
         if (!x_item)
         {
             Wt::log("error") << "-> makeConnectionPool(" << filePath.toStdString() << ") Missing XML Element: defaultTheme";
-            return 1;
+            return false;
         }
         std::string defaultTheme(x_item->value(), x_item->value_size());
         myDefaultTheme[domainHost] = defaultTheme;
@@ -257,19 +244,23 @@ int main(int argc, char **argv)
 
         server.setServerConfiguration(argc, argv, WTHTTP_CONFIGURATION);
 
-        BlogSession::configureAuth();
+        #ifdef BLOGMAN
+            BlogSession::configureAuth();
+        #endif // BLOGMAN
 
         // Note: this is in the root with executable
         // ./app_root/domains.xml
-        if (!makeConnectionPool(server.appRoot().append("domains.xml").c_str()))
+        if (!getConnectionPoolInfo(server.appRoot().append("domains.xml").c_str()))
         {
-            return 1; // Fix 404
+            return false; // Fix 404
         }
 
         server.addEntryPoint(Wt::Application, boost::bind(&createWWHomeApplication,  _1), "", "favicon.ico");
-        // rss feed
-        BlogRSSFeed rssFeed;
-        server.addResource(&rssFeed, "/" + rootPrefix + "/blog/feed/");
+        #ifdef BLOGMAN
+            // rss feed
+            BlogRSSFeed rssFeed;
+            server.addResource(&rssFeed, "/" + rootPrefix + "/blog/feed/");
+        #endif // BLOGMAN
 
         //SimpleChatServer chatServer(server);
         //server.addEntryPoint(Wt::Application, boost::bind(createApplication, _1, boost::ref(chatServer)), "/chat");
